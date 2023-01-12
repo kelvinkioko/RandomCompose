c46656f1fe22890bb5efe9244e93207972ab048d
Kelvin Kioko <kelvinkioko@Kelvins-MacBook-Pro-2.local>
2023-01-12 16:58:05 +0300

    Costomizing text composable

--- app/src/main/java/com/graph/randomcompose/MainActivity.kt
+++ app/src/main/java/com/graph/randomcompose/MainActivity.kt
@@ -3,14 +3,16 @@ package com.graph.randomcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.graph.randomcompose.ui.theme.RandomComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +34,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingButton() {
-    Button(onClick = { /*TODO*/ }) {
+    Button(
+        onClick = { /*TODO*/ },
+        modifier = Modifier
+            .width(200.dp)
+            .height(80.dp)
+    ) {
         Greeting(name = "Kelvin ")
         Greeting(name = "Kioko")
     }
@@ -40,10 +47,16 @@ fun GreetingButton() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier
+            .fillMaxWidth(0.5f),
+        style = MaterialTheme.typography.h5,
+        fontWeight = FontWeight.SemiBold
+    )
 }
 
-@Preview(showBackground = true, showSystemUi = false)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     RandomComposeTheme {
