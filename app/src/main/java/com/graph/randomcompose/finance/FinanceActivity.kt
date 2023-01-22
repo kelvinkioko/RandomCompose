899ba828b72d76d296be6aadd65b22594b8a66d5
Kelvin Kioko DVTMC <kelvinmkioko@gmail.com>
2023-01-22 14:51:45 +0300

    Created the finance activity

---
+++ app/src/main/java/com/graph/randomcompose/finance/FinanceActivity.kt
@@ -0,0 +1,42 @@
+package com.graph.randomcompose.finance
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.graph.randomcompose.GreetingButton
+import com.graph.randomcompose.ui.theme.RandomComposeTheme
+
+class FinanceActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RandomComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    FinanceHomeScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun FinanceHomeScreen() {
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun FinanceHomeScreenPreview() {
+    RandomComposeTheme {
+        FinanceHomeScreen()
+    }
+}
