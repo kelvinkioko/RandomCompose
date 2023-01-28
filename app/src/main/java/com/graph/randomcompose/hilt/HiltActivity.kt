962c22117afa3e23a53df186c8c29815722da510
Kelvin Kioko DVTMC <kelvinmkioko@gmail.com>
2023-01-28 16:26:03 +0300

    Entry point

---
+++ app/src/main/java/com/graph/randomcompose/hilt/HiltActivity.kt
@@ -0,0 +1,76 @@
+package com.graph.randomcompose.hilt
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.graph.randomcompose.ui.theme.RandomComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RandomComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    HiltGreeting("Hello Student")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HiltGreetingButton() {
+    Button(
+        onClick = { /*TODO*/ },
+        modifier = Modifier
+            .width(200.dp)
+            .height(80.dp)
+    ) {
+        HiltGreeting(name = "Kelvin ")
+    }
+}
+
+@Composable
+fun HiltGreeting(name: String) {
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier
+            .border(2.dp, Color.Green)
+            .padding(50.dp)
+            .border(2.dp, Color.Red)
+            .padding(50.dp)
+        ,
+        style = MaterialTheme.typography.h5,
+        fontWeight = FontWeight.SemiBold
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultHiltPreview() {
+    RandomComposeTheme {
+        HiltGreetingButton()
+    }
+}
