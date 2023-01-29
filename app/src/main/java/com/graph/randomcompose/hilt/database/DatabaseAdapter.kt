69d05d550b09d07c841203f9ad5d15ba1bf46017
Kelvin Kioko DVTMC <kelvinmkioko@gmail.com>
2023-01-29 09:59:57 +0300

    Injected Context

--- app/src/main/java/com/graph/randomcompose/hilt/database/DatabaseAdapter.kt
+++ app/src/main/java/com/graph/randomcompose/hilt/database/DatabaseAdapter.kt
@@ -1,11 +1,17 @@
 package com.graph.randomcompose.hilt.database
 
+import android.content.Context
+import dagger.hilt.android.qualifiers.ActivityContext
 import javax.inject.Inject
 
 
-class DatabaseAdapter @Inject constructor(var databaseService: DatabaseService) {
+class DatabaseAdapter @Inject constructor(
+    @ActivityContext private val context: Context,
+    var databaseService: DatabaseService
+) {
     fun log(message: String) {
         println("@@@ Database adapter message: $message")
         databaseService.log(message = message)
+        println("@@@ Database adapter context: $context")
     }
 }
